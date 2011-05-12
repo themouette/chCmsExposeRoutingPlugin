100d5d80da64165d91d10bc1c1d3f68be53c0421
Julien MUETTON <julien_muetton@carpe-hora.com>
2011-05-13 01:56:14 +0200

    renamed js register to connect. passes tests.
    modified:   modules/chCmsExposeRouting/templates/indexSuccess.js.php
    modified:   web/js/routing.js

--- modules/chCmsExposeRouting/templates/indexSuccess.js.php
+++ modules/chCmsExposeRouting/templates/indexSuccess.js.php
@@ -1,8 +1,11 @@
-/* list of routes */
+/* 
+list of routes.
+auto generated.
+*/
 
 Routing.prefix = <?php echo json_encode($prefix); ?>; 
 <?php foreach($exposed_routes as $id => $route): ?>
-Routing.register(<?php echo json_encode($id) ?>, <?php echo json_encode($route->getPattern()) ?>);
+Routing.connect(<?php echo json_encode($id) ?>, <?php echo json_encode($route->getPattern()) ?>);
 <?php endforeach; ?>
 
 <?php 
