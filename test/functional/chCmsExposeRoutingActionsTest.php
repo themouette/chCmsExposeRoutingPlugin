3fb7cb1e2d754d6a6824b6224928816c662611ac
Julien MUETTON <julien_muetton@carpe-hora.com>
2011-05-14 01:53:58 +0200

    Added php functionnal tests
    new file:   test/bin/prove.php
    new file:   test/bootstrap/functional.php
    new file:   test/bootstrap/unit.php
    new file:   test/fixtures/project/apps/frontend/config/app.yml
    new file:   test/fixtures/project/apps/frontend/config/cache.yml
    new file:   test/fixtures/project/apps/frontend/config/factories.yml
    new file:   test/fixtures/project/apps/frontend/config/filters.yml
    new file:   test/fixtures/project/apps/frontend/config/frontendConfiguration.class.php
    new file:   test/fixtures/project/apps/frontend/config/routing.yml
    new file:   test/fixtures/project/apps/frontend/config/security.yml
    new file:   test/fixtures/project/apps/frontend/config/settings.yml
    new file:   test/fixtures/project/apps/frontend/config/view.yml
    new file:   test/fixtures/project/apps/frontend/lib/myUser.class.php
    new file:   test/fixtures/project/apps/frontend/templates/layout.php
    new file:   test/fixtures/project/cache/frontend/test/test/sessions/a2101b96ca22d3030418a223de15f5fe.session
    new file:   test/fixtures/project/config/ProjectConfiguration.class.php
    new file:   test/fixtures/project/config/properties.ini
    new file:   test/fixtures/project/config/rsync_exclude.txt
    new file:   test/fixtures/project/config/settings.yml
    new file:   test/fixtures/project/data/fixtures/fixtures.yml
    new file:   test/fixtures/project/lib/form/BaseForm.class.php
    new file:   test/fixtures/project/symfony
    new file:   test/fixtures/project/test/bootstrap/functional.php
    new file:   test/fixtures/project/test/bootstrap/unit.php
    new file:   test/fixtures/project/web/.htaccess
    new file:   test/fixtures/project/web/css/main.css
    new file:   test/fixtures/project/web/robots.txt
    new file:   test/functional/chCmsExposeRoutingActionsTest.php

---
+++ test/functional/chCmsExposeRoutingActionsTest.php
@@ -0,0 +1,116 @@
+<?php
+
+if (!isset($_SERVER['SYMFONY']))
+{
+  $_SERVER['SYMFONY'] = '../../lib/vendor/symfony/lib/';
+}
+
+include dirname(__FILE__).'/../bootstrap/functional.php';
+
+$browser = new sfTestFunctional(new sfBrowser());
+
+$browser
+  ->info("check default config")
+    ->get('/js/app/routes.js')
+    
+    //check routing is OK
+    ->with('request')->begin()
+      ->isMethod('get')
+      ->isParameter('module', 'chCmsExposeRouting')
+      ->isParameter('action', 'index')
+    ->end()
+    
+    // check the response
+    ->with('response')->begin()
+      ->isStatusCode(200)
+      ->isHeader('content-type', 'application/javascript')
+      ->contains('Routing.prefix = "\/index.php";')
+      ->contains('Routing.variablePrefix = [":"]; ')
+      ->contains('Routing.variableSuffix = ""; ')
+      ->contains('Routing.segmentSeparators = ["\/","."]; ')
+      ->contains('Routing.defaults = {"module":"default","action":"index","sf_culture":"en"}; ')
+      ->contains('Routing.connect("exposed_route", "\/exposed", {"module":"test","action":"test"});')
+      ->contains('Routing.connect("default", "\/:module\/:action\/*", []);')
+      ->contains('Routing.connect("default_index", "\/:module", {"action":"index"});')
+      ->matches('!/private_route/')
+    ->end()
+    // end of get /js/app/routes.js
+    ->get('/')
+    
+    //check routing is OK
+    ->with('request')->begin()
+      ->isMethod('get')
+      ->isParameter('module', 'default')
+      ->isParameter('action', 'index')
+    ->end()
+    
+    // check the response
+    ->with('response')->begin()
+      ->isStatusCode(200)
+      ->isValid()
+      ->checkElement('head script[src=/index.php/js/app/routes.js]', 1)
+      ->checkElement('head script[src=/chCmsExposeRoutingPlugin/js/routing.js]', 1)
+    ->end()
+    // end of get /
+  // end of block check url is accessible
+;
+
+//change environment
+$configuration = ProjectConfiguration::getApplicationConfiguration($app, 'test_without_auto_discover',isset($debug) ? $debug : true);
+sfContext::createInstance($configuration);
+$browser = new sfTestFunctional(new sfBrowser());
+
+$browser
+  ->info("check with auto_discover to off")
+    ->get('/js/app/routes.js')
+    
+    // check the response
+    ->with('response')->begin()
+      ->isStatusCode(200)
+      ->isHeader('content-type', 'application/javascript')
+      ->matches('!/exposed_route/')
+    ->end()
+    // end of get /js/app/routes.js
+  // end of block check with auto_discover to off
+;
+
+//change environment
+$configuration = ProjectConfiguration::getApplicationConfiguration($app, 'test_without_auto_register',isset($debug) ? $debug : true);
+sfContext::createInstance($configuration);
+$browser = new sfTestFunctional(new sfBrowser());
+
+$browser
+  ->info("check with no_register_routes")
+    ->get('/js/app/routes.js')
+    
+    //check routing is OK
+    ->with('request')->begin()
+      ->isMethod('get')
+    ->end()
+    
+    // check the response
+    ->with('response')->begin()
+      ->isStatusCode(404)
+    ->end()
+    // end of get /js/app/routes.js
+
+    ->get('/')
+    
+    //check routing is OK
+    ->with('request')->begin()
+      ->isMethod('get')
+      ->isParameter('module', 'default')
+      ->isParameter('action', 'index')
+    ->end()
+    
+    // check the response
+    ->with('response')->begin()
+      ->isStatusCode(200)
+      ->isValid()
+      ->checkElement('head script[src=/index.php/js/app/routes.js]', 0)
+      ->checkElement('head script[src=/chCmsExposeRoutingPlugin/js/routing.js]', 0)
+    ->end()
+    // end of get /
+  // end of block check with no_register_routes
+;
+// vim: ft=symfony.php.sftest
