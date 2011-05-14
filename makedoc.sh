844a82b04d7ce3b63750d47ff4b25fa79afc7629
Julien MUETTON <julien_muetton@carpe-hora.com>
2011-05-14 02:15:15 +0200

    now generate php doc with docblox
    modified:   makedoc.sh

--- makedoc.sh
+++ makedoc.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+SCRIPTPATH=`readlink -f $0`;
+PLUGIN_PATH=`dirname $SCRIPTPATH`;
+
+# js doc, relies on [dox](http://visionmedia.github.com/dox/)
 dox --title chCmsExposeRoutingPlugin --private --intro doc/header.md web/js/routing.js > doc/js/index.html
+
+# php doc, relies on [docblox](http://www.docblox-project.org/)
+docblox run -d $PLUGIN_PATH -t doc/php/ -i doc/,test/,web/
