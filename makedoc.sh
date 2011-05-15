2e6d12ec464fad7f4627896192d2677606f3cf42
Julien MUETTON <julien_muetton@carpe-hora.com>
2011-05-15 19:14:55 +0200

    updated doc

--- makedoc.sh
+++ makedoc.sh
@@ -6,4 +6,4 @@ PLUGIN_PATH=`dirname $SCRIPTPATH`;
 dox --title chCmsExposeRoutingPlugin --private --intro doc/header.md web/js/routing.js > doc/js/index.html
 
 # php doc, relies on [docblox](http://www.docblox-project.org/)
-docblox run -d $PLUGIN_PATH -t doc/php/ -i doc/,test/,web/
+docblox run --title "chCmsExposeRoutingPlugin api documentation" -d $PLUGIN_PATH -t doc/php/ -i doc/,test/,web/
