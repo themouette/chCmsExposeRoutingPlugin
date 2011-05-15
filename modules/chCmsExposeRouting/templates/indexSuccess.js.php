8fef86a5d380fff37d7b5c8f0ee011df4bbc2219
Julien MUETTON <julien_muetton@carpe-hora.com>
2011-05-15 21:17:57 +0200

    typo fix

--- modules/chCmsExposeRouting/templates/indexSuccess.js.php
+++ modules/chCmsExposeRouting/templates/indexSuccess.js.php
@@ -9,7 +9,7 @@ Routing.variablePrefix = <?php echo json_encode($options['variable_prefixes']); 
 Routing.variableSuffix = <?php echo json_encode($options['suffix']); ?>; 
 Routing.segmentSeparators = <?php echo json_encode($options['segment_separators']); ?>; 
 Routing.defaults = <?php echo json_encode(chCmsExposeRoutingTools::filterParameters($defaultParameters)); ?>; 
-Routing.csrf = <?php echo json_encode($csrf); ?>
+Routing.csrf = <?php echo json_encode($csrf); ?>;
 <?php foreach($exposed_routes as $id => $route): ?>
 Routing.connect(<?php echo json_encode($id); ?>
 , <?php echo json_encode($route->getPattern()); ?>
