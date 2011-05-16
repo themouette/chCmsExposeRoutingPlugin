fcbde80082d27028f17c9853b7296b6c6b554566
Julien MUETTON <julien_muetton@carpe-hora.com>
2011-05-16 11:48:32 +0200

    added the ability to test in ci environment
    new file:   build.xml
    modified:   test/bin/prove.php

--- test/bin/prove.php
+++ test/bin/prove.php
@@ -1,8 +1,34 @@
 <?php
 
+if (count($argv) > 1 && !isset($_SERVER['SYMFONY']))
+{
+  $_SERVER['SYMFONY'] = $argv[1];
+}
+
+$xmlExport = false;
+if (count($argv) > 2)
+{
+  $xmlExport = $argv[2];
+}
+
+//default symfony look for project lib vendor
+if (!isset($_SERVER['SYMFONY']))
+{
+  $_SERVER['SYMFONY'] = '../../lib/vendor/symfony/lib/';
+}
+
 include dirname(__FILE__).'/../bootstrap/unit.php';
 
 $h = new lime_harness(new lime_output_color());
 $h->register(sfFinder::type('file')->name('*Test.php')->in(dirname(__FILE__).'/..'));
 
-exit($h->run() ? 0 : 1);
+$ret = $h->run() ? 0 : 1;
+
+// publish xml results
+if ($xmlExport)
+{
+  file_put_contents($xmlExport, $h->to_xml());
+}
+
+
+exit($ret);
